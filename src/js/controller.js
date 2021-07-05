fa3c38e84dd05fc1bec43b1bcf528378a283dbe4
helmifnandani <helmifnandani@gmail.com>
2021-07-05 11:11:56 +0700

    add new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,6 +133,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to');
+};
+
 const init = function () {
   // Subscriber. Dalam Publisher-Subscriber Pattern
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -142,6 +146,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
